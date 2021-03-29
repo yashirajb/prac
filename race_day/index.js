6208f75b61e9a837a9fbde9331ae3cf8ed3367e9
Yashira Baggett <Yashira@Yashiras-Air.attlocal.net>
2021-03-29 08:55:02 -0700

    initial excersise code

---
+++ race_day/index.js
@@ -0,0 +1,10 @@
+//race numbers are assigned randomly. 
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const earlyRegister = true;
+
+const runnerAge = 18; 
+
+//create a control flow statement that checks whether the runner is an adult and registered early
+
