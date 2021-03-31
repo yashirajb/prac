c128e9661462090d78fa843c07c8c1d3d5d1953e
Yashira Baggett <Yashira@Yashiras-Air.attlocal.net>
2021-03-31 08:38:30 -0700

    exercise code complete

---
+++ arr/index.js
@@ -0,0 +1,31 @@
+
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length);
+
+console.log(secretMessage.pop());
+
+console.log(secretMessage.length);
+
+console.log(secretMessage.push('to', 'Program'));
+
+console.log(secretMessage);
+
+console.log(secretMessage[7] = 'right');
+
+console.log(secretMessage);
+
+console.log(secretMessage.shift());
+
+console.log(secretMessage);
+
+secretMessage.unshift('Programming');
+
+console.log(secretMessage);
+
+secretMessage.splice(6, 5, 'know');
+
+console.log(secretMessage);
+
+console.log(secretMessage.join());
